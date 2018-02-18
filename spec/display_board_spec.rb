61a8d9c4d0b3191211fd4d94d57cb9c8b2846a8a
LaurenceBurrows Burrows <lbur092@aucklanduni.ac.nz>
2018-02-18 23:18:52 +0000

    automatically backed up by learn

--- spec/display_board_spec.rb
+++ spec/display_board_spec.rb
@@ -145,7 +145,7 @@ describe "#display_board in 'lib/display_board.rb" do
 
       # Define the board with values that should create the desired output
       # *** Edit the line below ***
-      board = ["X", "X", "X", "X", "X", "X", "X", "X", "X"] 
+      board = ["X", "X", "X", "X", "X", "X", "X", "X", "X"]
 
       # Don't touch the following lines.
       output = capture_puts{ display_board(board) } if defined?(display_board)
@@ -172,7 +172,7 @@ describe "#display_board in 'lib/display_board.rb" do
       expect(rows[4]).to eq(" X | X | X ")
 
       # *** Comment the line below by adding a # at the line start ***
-      expect(true).to be(true)
+      #expect(true).to be(true)
     end
 
     it 'prints an entire board full of Os' do
